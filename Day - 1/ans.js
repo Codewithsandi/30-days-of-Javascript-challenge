10931d4c3d4fe56e7cffb7ac55a9782a3db72b0b
Codewithsandi <codersandipan.com>
2024-07-18 12:32:34 +0530

    day 1 complete

---
+++ Day - 1/ans.js
@@ -0,0 +1,56 @@
+//? Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+var number = 10;
+
+console.log(number);
+
+//? Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+let name = "Sandipan Kundu";
+
+console.log(name);
+
+//? Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const bool = true;
+
+console.log(bool);
+
+//? Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+let num = 10;
+let user_name = "Sandi";
+let boolean = false;
+let obj = {
+  f_name: "Sandipan",
+  l_name: "Kundu",
+  stream: "MCA",
+  city: "Basirhat",
+};
+let arr = [1,2,3];
+
+console.log(typeof num);
+console.log(typeof user_name);
+console.log(typeof boolean);
+console.log(typeof obj);
+console.log(typeof arr);
+
+//? Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+let num2 = 10;
+
+console.log(num2);
+
+num2 = 25;
+
+console.log(num2);
+
+//? Task 6: Try reassigning a variable declared with const and observe the error.
+
+const num3 = 10;
+
+console.log(num3);
+
+// num3 = 25;
+
+// console.log(num3);
