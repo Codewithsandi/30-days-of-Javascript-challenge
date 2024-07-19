bdef95c5f78b66e14c2111c7ef2c8175401436c0
Codewithsandi <codersandipan.com>
2024-07-19 13:06:58 +0530

    day 2 complete

---
+++ Day - 2/ans.js
@@ -0,0 +1,117 @@
+//? Task 1: Write a program to add two numbers and log the result to the console.
+
+// console.log(10+20);
+
+//? Task 2: Write a program to subtract two numbers and log the result to the console.
+
+// console.log(20-11);
+
+//? Task 3: Write a program to multiply two numbers and log the result to the console.
+
+// console.log(20*4);
+
+//? Task 4: Write a program to divide two numbers and log the result to the console.
+
+// console.log(20/2);
+
+//? Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+// console.log(11%3);
+
+//? Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let num1 = 10;
+let add_value_1 = 5;
+num1 += add_value_1;
+
+// console.log(num1);
+
+//? Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+
+let num2 = 10;
+let add_value_2 = 5;
+num1 += add_value_2;
+
+// console.log(num1);
+
+//? Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+
+let num3 = 10;
+let num4 = 15;
+
+if (num3 > num4) {
+    // console.log(num3);
+}else{
+    // console.log(num4);
+}
+
+let num5 = 1250;
+let num6 = 185;
+
+if (num5 < num6) {
+    // console.log(num5);
+}else{
+    // console.log(num6);
+}
+
+//? Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+
+let num7 = 5;
+
+if (num7 > 40 && num7 < 60) {
+    // console.log(num7 + " is present.");
+}else{
+    // console.log("404 not found.");
+}
+
+//? Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+let num8 = 10;
+let num9 = '10';
+
+if (num8 == num9 ) {
+    // console.log("equal");
+}else{
+    // console.log("Not equal");
+}
+
+if (num8 === num9) {
+    // console.log("equal");
+}else{
+    // console.log("Not equal");
+}
+
+//? Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+
+// check task 9
+
+//? Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+
+let num10 = 5;
+let num11 = 15;
+
+if (num10 > 1 || num11 < 10) {
+    // console.log("Done");
+}else{
+    // console.log("404 not found.");
+}
+
+//? Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+
+let bool = true
+
+if (!bool) {
+    // console.log("failed.");
+}else{
+    // console.log("Success.");
+}
+
+//? Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let num12 = -1; 
+
+console.log(
+
+    num12 >= 0 ? "Positive" : "Negative"
+    
+);
